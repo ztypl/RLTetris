448f2966975f7b19b7adb04f51427d9795897696
赵天宇 <ztypl@hotmail.com>
2018-10-23 00:56:55 +0800

    fix basic gui

--- game/game_gui.py
+++ game/game_gui.py
@@ -22,45 +22,60 @@ class Drawing:
 
 
 class Board(QFrame):
-    def __init__(self, parent, grid_size, width_blocks, height_blocks):
+    def __init__(self, parent, grid_size, width_blocks, height_blocks, border_width=5):
         self.grid_size = grid_size
+        self.border_width = border_width
         super().__init__(parent)
         self.core = BoardCore(width=width_blocks, height=height_blocks)
-        self.setFixedSize(grid_size * width_blocks, grid_size * height_blocks)
-        self.setFrameShape(QFrame.Box)
+        self.setFixedSize(grid_size * width_blocks + 2 * self.border_width,
+                          grid_size * height_blocks + 2 * self.border_width)
 
 
     def paintEvent(self, event):
+        super().paintEvent(event)
         painter = QPainter(self)
+        self.setStyleSheet("border:%dpx solid red;" % self.border_width)
         for i in range(self.core.height):
             for j in range(self.core.width):
                 val = self.core.get_value(i, j)
-                Drawing.draw_square(painter, j * self.grid_size, i * self.grid_size, val, self.grid_size)
+                Drawing.draw_square(painter,
+                                    j * self.grid_size + self.border_width,
+                                    i * self.grid_size + self.border_width,
+                                    val, self.grid_size)
 
         for x, y in self.core.get_active_shape():
-            val = self.core.active_shape.shape_id + 1
-            Drawing.draw_square(painter, y * self.grid_size, x * self.grid_size, val, self.grid_size)
+            if x >= 0 and y >= 0:
+                val = self.core.active_shape.shape_id + 1
+                Drawing.draw_square(painter,
+                                    y * self.grid_size + self.border_width,
+                                    x * self.grid_size + self.border_width,
+                                    val, self.grid_size)
 
 
 class NextBlockPanel(QFrame):
-    def __init__(self, parent, grid_size, shape):
+    def __init__(self, parent, grid_size, shape, border_width=5):
         self.grid_size = grid_size
         self.width_block = 4
         self.height_block = 4
         self.shape = shape
+        self.border_width = border_width
         super().__init__(parent)
-        self.setFrameShape(QFrame.Box)
-        self.setFixedSize(grid_size * self.width_block, grid_size * self.height_block)
+        self.setFixedSize(grid_size * self.width_block + 2 * self.border_width,
+                          grid_size * self.height_block + 2 * self.border_width)
+
 
     def set_shape(self, shape):
         self.shape = shape
 
     def paintEvent(self, event):
+        self.setStyleSheet("border:%dpx solid red;" % self.border_width)
         painter = QPainter(self)
         val = self.shape.shape_id + 1
         for x, y in self.shape.get_local_coord():
-            Drawing.draw_square(painter, (x + 1) * self.grid_size, (y + 2) * self.grid_size, val, self.grid_size)
-
+            Drawing.draw_square(painter,
+                                (y + 2) * self.grid_size + self.border_width,
+                                (3 - x) * self.grid_size + self.border_width,
+                                val, self.grid_size)
 
 
 class GameGUI(QMainWindow):
@@ -74,7 +89,6 @@ class GameGUI(QMainWindow):
     def __init__(self, speed=1000):
         super().__init__()
         self.speed = speed
-        self.is_next_shape = True
 
         # flag
         self.game_status = self.BEFORE_START    # 0：未开始 1：正在运行 2：暂停 3：游戏结束
@@ -89,14 +103,12 @@ class GameGUI(QMainWindow):
         self.init_gui()
         self.start()
 
-
     def init_gui(self, width_block=10, height_block=22, grid_size=20):
         center_widget = QWidget()
 
-
         h_layout = QHBoxLayout()
-        self.board = Board(center_widget, grid_size, width_block, height_block)
-        self.next_panel = NextBlockPanel(center_widget, grid_size, self.board.core.next_shape)
+        self.board = Board(None, grid_size, width_block, height_block)
+        self.next_panel = NextBlockPanel(None, grid_size, self.board.core.next_shape)
         h_layout.addWidget(self.board)
         h_layout.addWidget(self.next_panel)
 
@@ -104,15 +116,21 @@ class GameGUI(QMainWindow):
         self.setFocusPolicy(Qt.StrongFocus)
 
         self.setWindowTitle('Tetris')
-        center_widget.setFixedSize(self.board.width() + self.next_panel.width(), self.board.height())
-        # self.setFixedSize(self.board.width() + self.next_panel.width(), self.board.height())
+        center_widget.setFixedSize(self.board.width() + self.next_panel.width() + 30, self.board.height()+15)
+        self.setFixedSize(center_widget.width(), center_widget.height())
         center_widget.setLayout(h_layout)
+
         self.setCentralWidget(center_widget)
+
+
+        print(self.dockOptions())
+
         self.show()
 
     def start(self):
+        self.board.core.remove_lines()
         self.board.core.generate_next_shape()
-        self.is_next_shape = False
+        self.next_panel.set_shape(self.board.core.next_shape)
         self.game_status = self.RUNNING
         self.timer.start(self.speed, self)
         self.update()
